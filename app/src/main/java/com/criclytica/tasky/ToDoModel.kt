b7d4c1fc11b842e76777828d86648dc3e714674b
prateek-ml <prateekb1912@gmail.com>
2021-09-03 13:13:17 +0530

    Populate the model class with the required paramaters

--- app/src/main/java/com/criclytica/tasky/ToDoModel.kt
+++ app/src/main/java/com/criclytica/tasky/ToDoModel.kt
@@ -1,8 +1,17 @@
 package com.criclytica.tasky
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
-data class ToDoModel {
+data class ToDoModel(
+    var title: String,
+    var description: String,
+    var category: String,
+    var date: Long,
+    var time: Long,
+    var isDone: Boolean = false,
+    @PrimaryKey
+    var id: Long = 0
 
-}
+)
