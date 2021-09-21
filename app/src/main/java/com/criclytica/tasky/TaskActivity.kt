0704afa955722e2afa8889dc416ca1cbbfcefef3
prateek-ml <prateekb1912@gmail.com>
2021-09-21 09:01:37 +0530

    Define database builder for the app

--- app/src/main/java/com/criclytica/tasky/TaskActivity.kt
+++ app/src/main/java/com/criclytica/tasky/TaskActivity.kt
@@ -2,8 +2,19 @@ package com.criclytica.tasky
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 
+const val DB_NAME = "todo.db"
 class TaskActivity : AppCompatActivity() {
+
+    val db by lazy {
+        Room.databaseBuilder(
+            this,
+            AppDatabase::class.java,
+            DB_NAME
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task)
