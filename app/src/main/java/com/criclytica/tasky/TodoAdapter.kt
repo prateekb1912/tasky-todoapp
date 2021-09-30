9461e02008b4c0c496860cda2992df6527cfbbce
prateek-ml <prateekb1912@gmail.com>
2021-09-30 10:21:22 +0530

    Implement the members of the ToDo Adapter

--- app/src/main/java/com/criclytica/tasky/TodoAdapter.kt
+++ app/src/main/java/com/criclytica/tasky/TodoAdapter.kt
@@ -1,2 +1,23 @@
 package com.criclytica.tasky
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter(val list = List<ToDoModel>):RecyclerView.Adapter<TodoAdapter.TodoViewHolder>(){
+    class TodoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
