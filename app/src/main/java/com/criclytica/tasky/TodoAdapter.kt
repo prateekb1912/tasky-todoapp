12409e8942b1addd4585c95f5c64fdb3dbfc63b6
prateek-ml <prateekb1912@gmail.com>
2021-10-04 09:26:50 +0530

    Add color palette to add to the list of tasks page

--- app/src/main/java/com/criclytica/tasky/TodoAdapter.kt
+++ app/src/main/java/com/criclytica/tasky/TodoAdapter.kt
@@ -1,5 +1,6 @@
 package com.criclytica.tasky
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,17 +8,24 @@ import androidx.recyclerview.widget.RecyclerView
 class TodoAdapter(val list = List<ToDoModel>):RecyclerView.Adapter<TodoAdapter.TodoViewHolder>(){
     class TodoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
+        fun bind(toDoModel: ToDoModel) {
+            with(itemView) {
+                val colors = resources.getIntArray()
+            }
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        TODO("Not yet implemented")
+        return TodoViewHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_todo, parent, false))
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(list[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = list.size
+
+
 }
