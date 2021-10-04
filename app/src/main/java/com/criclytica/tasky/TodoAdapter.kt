abed749ec2abcfa87ce2b2ecf7e166ee358ddc25
prateek-ml <prateekb1912@gmail.com>
2021-10-04 09:40:39 +0530

    Complete the ToDO Recycler View adapter

--- app/src/main/java/com/criclytica/tasky/TodoAdapter.kt
+++ app/src/main/java/com/criclytica/tasky/TodoAdapter.kt
@@ -4,15 +4,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_task.*
+import kotlinx.android.synthetic.main.item_todo.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TodoAdapter(val list = List<ToDoModel>):RecyclerView.Adapter<TodoAdapter.TodoViewHolder>(){
     class TodoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
         fun bind(toDoModel: ToDoModel) {
             with(itemView) {
-                val colors = resources.getIntArray()
+                val colors = resources.getIntArray(R.array.color_arr)
+                val rand_col = colors[Random().nextInt(colors.size)]
+
+                viewColorTag.setBackgroundColor(rand_col)
+                txtShowTitle.text = toDoModel.title
+                txtShowTask.text = toDoModel.description
+                txtShowCategory.text = toDoModel.category
+
+                updateTime(toDoModel.time)
+                updateDate(toDoModel.date)
             }
         }
+
+        private fun updateTime(time: Long) {
+            val format = "h:mm a"
+
+            val sdf = SimpleDateFormat(format)
+            itemView.txtShowTime.text = sdf.format(Date(time))
+        }
+
+        private fun updateDate(date: Long) {
+            val format = "EEE, d MMM yyyy"
+
+            val sdf = SimpleDateFormat(format)
+            itemView.txtShowDate.text = sdf.format(Date(date))
+        }
+
+
 
     }
 
@@ -26,6 +55,5 @@ class TodoAdapter(val list = List<ToDoModel>):RecyclerView.Adapter<TodoAdapter.T
     }
 
     override fun getItemCount() = list.size
-
 
 }
